2160e888f9d95263892bf3117274e4887a63ffe4
mykola.bielousov <mykola.bielousov@pdisoftware.com>
2024-02-02 15:28:36 -0800

    test ng hidden

--- src/test/java/RunnerGoogleTestNG.java
+++ src/test/java/RunnerGoogleTestNG.java
@@ -1,20 +1,20 @@
-import io.cucumber.core.cli.Main;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.core.cli.Main;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-        features = "classpath:features/",
-        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
-        tags = "@googleFinance")
-public class RunnerGoogleTestNG extends AbstractTestNGCucumberTests {
-    public static void main(String[] args) {
-        Main.main(new String[]{
-                "-g", "com.selenium.steps",
-                "-p", "pretty",
-                "-p", "html:target/cucumber-reports.html",
-                "-p", "json:target/cucumber.json",
-                "--tags", "@googleFinance",
-                "classpath:features/"
-        });
-    }
-}
+//@CucumberOptions(
+//        features = "classpath:features/",
+//        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+//        tags = "@googleFinance")
+//public class RunnerGoogleTestNG extends AbstractTestNGCucumberTests {
+//    public static void main(String[] args) {
+//        Main.main(new String[]{
+//                "-g", "com.selenium.steps",
+//                "-p", "pretty",
+//                "-p", "html:target/cucumber-reports.html",
+//                "-p", "json:target/cucumber.json",
+//                "--tags", "@googleFinance",
+//                "classpath:features/"
+//        });
+//    }
+//}
